51245cbb442dc7cc888118f771f4693084f3983d
socialruins <cwbuckner84@yahoo.com>
2024-01-05 15:03:05 -0500

    meh

--- test-peer.js
+++ test-peer.js
@@ -1,23 +1,30 @@
 var raptoreumcore = require("@socialruins/raptoreumcore-lib");
-var _ = raptoreumcore.deps._;
-var P2P = require("./");
-var Peer = P2P.Peer;
-var EventEmitter = require("events").EventEmitter;
-var Messages = P2P.Messages;
-var messages = new Messages();
 var Networks = raptoreumcore.Networks;
+var P2P = require("./");
+var Pool = P2P.Pool;
+var Messages = P2P.Messages;
+var Peer = P2P.Peer;
 
-var peer = new Peer({
-  host: "localhost",
-  port: 10226,
-  network: Networks.mainnet,
-});
+var peer = new Peer({ host: "127.0.0.1", port: 8484 });
 
 peer.on("ready", function () {
-  // peer info
-  console.log(peer.version, peer.subversion, peer.bestHeight);
+  console.log("Connected " + peer.version, peer.subversion, peer.bestHeight);
 });
 peer.on("disconnect", function () {
-  console.log("connection closed");
+  console.log("disconnected");
 });
+
+peer.on("connect", function () {
+  console.log("Connected " + peer.version, peer.subversion, peer.bestHeight);
+});
+
+//loop through peer notifications
+peer.on("inv", function (message) {
+  var nodeMsg = message.inventory;
+  var nodeMsgLen = nodeMsg.length;
+  for (var i = 0; i < nodeMsgLen; i++) {
+    console.log(nodeMsg[i].hash);
+  }
+});
+//connect
 peer.connect();
